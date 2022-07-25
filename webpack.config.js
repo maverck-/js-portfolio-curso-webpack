31083722d4bc158b343e96dc0306ade8ffd2953a
Maverick Gayoso <maverick.gayoso@gmail.com>
2022-07-25 10:25:24 -0400

    clase de preprocesadores y corrección de errores con sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPluging = require('mini-css-extract-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -8,5 +11,46 @@ module.exports = {
   },
   resolve: {
     extensions: [".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        use: {
+          loader: "babel-loader",
+        },
+        // Exclude permite omitir archivos o carpetas especificas
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?css|.styl$/i,
+        use: [MiniCssExtractPluging.loader, 
+          "css-loader",
+          "stylus-loader"
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // CONFIGURACIÓN DEL PLUGIN
+      inject: true, // INYECTA EL BUNDLE AL TEMPLATE HTML
+      template: "./public/index.html", // LA RUTA AL TEMPLATE HTML
+      filename: "./index.html", // NOMBRE FINAL DEL ARCHIVO
+    }),
+    new MiniCssExtractPluging(),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
   },
 };
