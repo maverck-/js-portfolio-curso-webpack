ba0fa193bb6acf4bf64f82703ffecbaa62513683
Maverick Gayoso <maverick.gayoso@gmail.com>
2022-07-26 11:11:28 -0400

    se agrega asset modules para manejo de fuentes e imágenes, se elimina copy plugin de la config

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@ const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPluging = require('mini-css-extract-plugin');
 const TerserPlugin = require("terser-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
+    // assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   resolve: {
     extensions: [".js"],
@@ -24,10 +26,21 @@ module.exports = {
       },
       {
         test: /\.s?css|.styl$/i,
-        use: [MiniCssExtractPluging.loader, 
-          "css-loader",
-          "stylus-loader"
-        ],
+        use: [MiniCssExtractPluging.loader, "css-loader", "stylus-loader"],
+      },
+      {
+        test: /\.png/,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/images/[hash][ext][query]", // Directorio de salida
+        },
+      },
+      {
+        test: /\.(woff|woff2)$/i, // Tipos de fuentes a incluir
+        type: "asset/resource", // Tipo de módulo a usar (este mismo puede ser usado para archivos de imágenes)
+        generator: {
+          filename: "assets/fonts/[hash][ext][query]", // Directorio de salida
+        },
       },
     ],
   },
@@ -39,6 +52,14 @@ module.exports = {
       filename: "./index.html", // NOMBRE FINAL DEL ARCHIVO
     }),
     new MiniCssExtractPluging(),
+    // new CopyPlugin({
+    //   patterns: [
+    //     {
+    //       from: path.resolve(__dirname, "src", "assets/images"),
+    //       to: "assets/images",
+    //     },
+    //   ],
+    // }),
   ],
   optimization: {
     minimize: true,
